f75d2cb754a3061c880ea338f2eb151cbe891605
airyland <airyland@qq.com>
2016-07-08 08:12:15 +0800

    Add Vux demo

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Home from './components/HelloFromVux'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import VueRouter from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+Vue.use(VueRouter){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+const router = new Router(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+router.map({
+	'/': {
+    component: Home{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+	}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+router.start(App, '#app'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
